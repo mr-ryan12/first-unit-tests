39b98ee6567bfac4d86ae72606c4dc1343bc31a1
Ryan McBride <mr.ryan444@gmail.com>
2021-10-14 21:27:40 -0600

    Pass functions tests

--- functions.js
+++ functions.js
@@ -1,9 +1,31 @@
 // functions.js
-function addTwoNumbers() {
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+function sayHello(name) {
+  if (name === 'Will') {
+    return `No more testing ${name}!`
+  }
+
+  return `Hi there ${name}!`;
+}
+
+function buildCar(color, type) {
+  var car = {
+    color: color,
+    type: type
+  }
+
+  if (color === undefined && type === undefined) {
+    return car = {};
+  }
+
+  return car;
 }
 
 module.exports = {
-    addTwoNumbers,
-    // sayHello,
-    // buildCar
+  addTwoNumbers,
+  sayHello,
+  buildCar
 }
